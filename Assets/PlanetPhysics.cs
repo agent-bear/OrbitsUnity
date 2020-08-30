896e5110978d49a7308005f9ad0014b920245f7f
agent-bear <mathijs.vankemenade@live.nl>
2020-08-30 16:08:08 +0200

    .

--- Assets/PlanetPhysics.cs
+++ Assets/PlanetPhysics.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetPhysics : MonoBehaviour
+public class planetPhysics : MonoBehaviour
 {
+    float mass;
+    public Vector3 initialVelocity;
+    Vector3 currentVelocity;
+    public GameObject[] allBodies;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        mass = GetComponent<Rigidbody>().mass;
+        currentVelocity = initialVelocity;
+        gameObject.tag ="celestialBody";
+        allBodies = GameObject.FindGameObjectsWithTag("celestialBody");
+        print(this);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void FixedUpdate(){
+
+        foreach(var body in allBodies){
+
+            if(body != this){
+                float distanceSqrd = Mathf.Round((body.GetComponent<Rigidbody>().position - GetComponent<Rigidbody>().position).sqrMagnitude);
+
+                Vector3 forceDir = (body.GetComponent<Rigidbody>().position - GetComponent<Rigidbody>().position).normalized;
+
+                Vector3 acceleration = (forceDir * universeController.gravConst * body.GetComponent<Rigidbody>().mass) / distanceSqrd;
+
+                currentVelocity += acceleration * universeController.timeStep;
+                print(forceDir);
+                print(universeController.gravConst);
+                print(body.GetComponent<Rigidbody>().mass);
+                print(distanceSqrd);
+                GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + currentVelocity * universeController.timeStep);
+                
+            }
+        }
     }
 }
---
+++ Assets/universeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class universeController : MonoBehaviour{
+
+    public const float gravConst = 0.0001f;
+    public const float timeStep = 0.01f;
+}
