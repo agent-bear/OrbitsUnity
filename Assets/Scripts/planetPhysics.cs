36ea27d252ab3f7850be8897031f1d0777ae77fa
agent-bear <mathijs.vankemenade@live.nl>
2020-09-03 17:09:03 +0200

    updated physics system, added textures

---
+++ Assets/Scripts/planetPhysics.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class planetPhysics : MonoBehaviour{
+
+    public Vector3 initialVelocity;
+    public Vector3 planetRotation;
+    public Rigidbody rb;
+    const float constant = 0.02f;
+
+    public static List<planetPhysics> physicsBodies;
+
+    void OnEnable(){
+        if( physicsBodies == null){
+            physicsBodies = new List<planetPhysics>();
+        }
+
+        physicsBodies.Add(this);
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+    }
+
+    void OnDisable(){
+        physicsBodies.Remove(this);
+    }
+
+    void Start(){
+        rb.AddForce(initialVelocity, ForceMode.VelocityChange);
+        rb.AddTorque(planetRotation, ForceMode.VelocityChange);
+    }
+
+    void FixedUpdate(){
+        foreach(planetPhysics body in physicsBodies ){
+            if(body != this){
+                simulateGravity(body);
+                //print(body);
+            }
+            
+        }
+    }
+
+    void simulateGravity(planetPhysics body){
+        Vector3 difference = body.rb.position - rb.position;
+        Vector3 direction = difference.normalized;
+        float distance = difference.sqrMagnitude;
+        float attractionStr = constant * (rb.mass * body.rb.mass) / distance;
+
+        Vector3 attraction = direction * attractionStr;
+
+        rb.AddForce(attraction);
+    }
+}
+
--- Assets/universeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class universeController : MonoBehaviour{
-
-    public const float gravConst = 1f;
-    public const float timeStep = 0.01f;
-}
